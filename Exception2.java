b2ddd2c46a384481da66f6fc482f8a3a9dea57da
tiger-1436 <waghgopal0007@gmail.com>
2023-01-08 11:55:48 +0530

    first commit

---
+++ Exception2.java
@@ -0,0 +1,22 @@
+class Exception2 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			
+		}
+		catch (Exception e)
+		{
+		}
+		catch (ArithmeticException ae)
+		{
+
+		}
+		catch (NullPointerException npe)
+		{
+
+		}
+		
+	}
+}
---
+++ Untitled1.java
@@ -0,0 +1,27 @@
+class Exception1 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{  
+			int a=Integer.parseInt(args[0]);
+			int b=Integer.parseInt(args[1]);
+           
+		   int c=a/b;
+		   System.out.println(c);
+
+			
+		}
+		catch (ArrayIndexOutOfBoundsException aiob)
+		{ System.our.println("please pass atleast two interger vslues");
+		}
+		catch (NumberFormatException nfe)
+		{
+           System.out.println("please pass only interger values");
+		}
+		catch (ArithmeticException ae)
+		{
+           System.out.println("please do not pass 2nd value ZERO");
+		}
+	}
+}
